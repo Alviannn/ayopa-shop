806ade64fed740f6419195278ba341388ad9f2cb
Gafiliano Gapili <gafiliano2@gmail.com>
2022-06-25 00:31:39 +0700

    add comment

--- src/com/github/argajuvi/database/Database.java
+++ src/com/github/argajuvi/database/Database.java
@@ -13,6 +13,28 @@ public class Database {
 
     private Connection conn;
 
+    /**
+     * <h1>Singleton Design Pattern</h1>
+     *
+     * <h2>Why we use it?</h2>
+     * 1. We think it is the most easiest and the simplest design pattern to be implemented.<br>
+     * 2. It is easy to understand.<br>
+     * 3. Singleton is a Creational Design Pattern and we have that exact object creation case.<br>
+     * 4. We have to create database instance, which is one of the common example from Singleton.<br><br>
+     *
+     * <h2>Explanation</h2>
+     * According to this <a href="https://refactoring.guru/design-patterns/singleton">source</a>, Singleton
+     * is a pattern that will only let us create no more than one instance. Since our project includes the
+     * creation of database, of course we won't create multiple databases or else our program will be run into
+     * confusion as to which data source should be used; and there is no point for creating multiple databases.
+     * As for the implementation, the concept is <b>the instance will be created if there is no existing instance</b>.
+     * To accommodate this concept, the function for object creation need to be set as {@code public static} so
+     * it can be accessed globally while the instance in question could remain {@code private}.<br><br>
+     *
+     * <h2>Purpose</h2>
+     * The only purpose of this function is <b>to get the Database instance</b>, with condition it
+     * will be created if it's not exists and will be returned the existing instance if it exists.
+     */
     public static Database getInstance() {
         if (instance == null) {
             instance = new Database();
--- src/com/github/argajuvi/models/product/ProductFactory.java
+++ src/com/github/argajuvi/models/product/ProductFactory.java
@@ -2,6 +2,27 @@ package com.github.argajuvi.models.product;
 
 import java.sql.Date;
 
+/**
+ * <h1>Factory Design Pattern</h1>
+ *
+ * <h2>Why we use it?</h2>
+ * We decided to use Factory Design Pattern because:<br>
+ * 1. We think it is one of the easiest pattern to be implemented.<br>
+ * 2. Our project contains models that is suitable for <b>Creational Design Pattern.</b><br><br>
+ *
+ * <h2>Explanation</h2>
+ * From several types of Factory Pattern in
+ * <a href="https://programhappy.net/2021/10/19/factory-design-pattern/">this link</a>,
+ * we use the <b>Factory</b> one considering our products: {@link com.github.argajuvi.models.product.BookProduct},
+ * {@link com.github.argajuvi.models.product.ClothingProduct}, and {@link com.github.argajuvi.models.product.FoodProduct}
+ * inherit from the same parent class {@link com.github.argajuvi.models.product.Product} but having <b>different constructor parameter</b>.
+ * Moreover, we found out similar Factory class that we want to implement from
+ * <a href="https://docs.oracle.com/javase/7/docs/api/javax/swing/BorderFactory.html">Java Swing</a>.<br><br>
+ *
+ * <h2>Purpose</h2>
+ * This class main purpose is to build different class of products that inherit from same parent class so that the client
+ * would not have to instantiate it themself. Basically we want to make it clean where the product should be produced.
+ */
 public class ProductFactory {
 
     public static Product createClothProduct(int id, String name, int price, char size) {
